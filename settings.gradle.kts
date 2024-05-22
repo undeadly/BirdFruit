53348fcfc4e02c7d1677a516349743f37c49d8bb
Cory Roy <cory.roy@lookout.com>
2024-05-22 10:49:50 -0700

    Update gradle repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,6 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -15,6 +9,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven() {
+            url = uri("https://maven.google.com")
+        }
     }
 }
 
